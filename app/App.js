bbbc77a0b7563400cb705574ce7ed764c08a9fb7
hanakamer <hanakamer@gmail.com>
2016-03-05 09:30:40 +0200

    initial commit

---
+++ app/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var Component = require ('./component')
+
+module.exports = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <Component />
+      </div>
+    )
+  }
+})
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  // will allow refering to JSX files without an extension
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }
+    ]
+  }
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallBack: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true
+      })
+    ],
+    devtool: 'eval-source-map',
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
