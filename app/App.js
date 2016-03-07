40bb47e542076675c5d89c5afca9f333f54ec13e
hanakamer <hanakamer@gmail.com>
2016-03-07 13:36:05 +0200

    range selection added

--- app/App.js
+++ app/App.js
@@ -1,34 +1,22 @@
 var React = require('react');
 var $ = require('jquery');
-var Table = require ('./Table')
+var Table = require ('./Table');
 
-var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",];
 var hours = ["12am", "3am", "6am", "9am", "12pm", "3pm", "6pm", "9pm"];
 var sections =["a", "b", "c"];
 
 
-
-module.exports = React.createClass({
+var App = React.createClass({
   getInitialState: function(){
     return {
       dragStart: 0,
       dragEnd: 0,
+      isMouseDown:false,
       isDragging: false,
-      oldumu:false
+      range:[]
     }
-  },
-  componentDidMount: function(){
-    // var x = document.getElementsByTagName("td");
-    // var txt = "";
-    // var i;
-    // for (i = 0; i < x.length; i++) {
-    //     txt = txt + "The index of Cell " + (i + 1) + " is: " + x[i].cellIndex + "<br>";
-    // }
-    // console.log(txt);
-    $("table td")
-        .mousedown(this.rangeMouseDown)
-        .mouseup(this.rangeMouseUp)
-        .mousemove(this.rangeMouseMove)
   },
   rangeMouseDown: function(e) {
     if (this.rightClick(e)) {
@@ -36,8 +24,9 @@ module.exports = React.createClass({
     } else {
       var allCells = $("#table td");
       this.setState({
-        dragStart:allCells.index($(this)),
-        isDragging: true
+        dragEnd:allCells.index(e.target),
+        dragStart: allCells.index(e.target),
+        isMouseDown: true
       });
       if (typeof e.preventDefault != 'undefined') {
         e.preventDefault();
@@ -51,32 +40,41 @@ module.exports = React.createClass({
     } else {
       var allCells = $("#table td");
       this.setState({
-        dragEnd: allCells.index($(this)),
-        isDragging: false
+        dragEnd: allCells.index(e.target),
+        isDragging: false,
+        isMouseDown: false
       });
-      if ( this.state.dragEnd != 0) {
+      if ( this.state.dragEnd != -1) {
         this.selectRange();
       }
-
       document.documentElement.onselectstart = function () { return true; };
     }
   },
   rangeMouseMove: function(e) {
+    if(this.state.isMouseDown) {
+      this.setState({
+        isDragging:true
+      });
+    }
     if(this.state.isDragging) {
       var allCells = $("#table td");
       this.setState({
-        dragEnd : allCells.index($(this))
+        dragEnd : allCells.index(e.target)
       });
       this.selectRange();
+    } else {
+
+      console.log(this.state.dragStart, this.state.dragEnd)
     }
   },
+
   selectRange: function() {
-    $("#table td").removeClass("selected");
+
     if(this.state.dragEnd + 1 < this.state.dragStart){
       $("#table td").slice(this.state.dragEnd, this.state.dragStart +1).addClass('selected');
     } else {
       $("#table td").slice(this.state.dragStart, this.state.dragEnd +1).addClass('selected');
-    }
+    };
   },
   rightClick: function(e) {
     if (e.which) {
@@ -86,16 +84,21 @@ module.exports = React.createClass({
     }
     return false;
   },
-  rowClickHandler: function(i, x){
-    this.setState({
-      oldumu:true
-    });
-  },
   render: function(){
     return(
       <div>
-        <Table tdays={days} thours={hours} tsections={sections} clickHandler={this.rowClickHandler}/>
+        <Table tdays={days}
+              thours={hours}
+              tsections={sections}
+              moveHandler={this.rangeMouseMove}
+              downHandler={this.rangeMouseDown}
+              upHandler={this.rangeMouseUp}
+              clickHandler={this.rangeMouseClick}
+              />
       </div>
     )
   }
-})
+});
+
+
+module.exports = App;
--- app/Table.js
+++ app/Table.js
@@ -1,14 +1,88 @@
 var React = require('react');
 var $ = require('jquery');
+let ClickOutHandler = require('react-onclickout');
+
+
+// class Table extends React.Component {
+//   handleMouseDown(i) {
+//     this.props.downHandler(i);
+//   }
+//   handleMouseClick(i) {
+//     this.props.clickHandler(i)
+//   }
+//   handleMouseUp(i) {
+//     this.props.upHandler(i);
+//   }
+//   handleMouseMove(i) {
+//     this.props.moveHandler(i);
+//   }
+//   onClickOut(e) {
+//     alert('user clicked outside of the component!');
+//   }
+//
+//   render() {
+//     var self = this;
+//     var _days = this.props.tdays;
+//     var _hours = this.props.thours;
+//     var _sections = this.props.tsections;
+//     var cells =[];
+//     _hours.map(function(hour, i){
+//       _sections.map(function(section, k){
+//         cells.push (hour+"-"+section)
+//       });
+//     });
+//     return (
+//       <div>
+//         <table id="table">
+//           <thead>
+//             <tr>
+//               <th></th>
+//               {_hours.map(function(hour, i){
+//                 return(
+//                   <th colSpan="3" key={i}>{hour}</th>
+//                 )
+//               })}
+//             </tr>
+//           </thead>
+//           <tbody>
+//           {_days.map(function(day,i){
+//             return (
+//               <tr className={day} key={i}>
+//                 <th>{day}</th>
+//                 {cells.map(function(cell,i){
+//                   return(
+//                     <td id={cell} key={i}
+//                     onMouseMove={self.handleMouseMove}
+//                     onMouseDown={self.handleMouseDown}
+//                     onMouseUp={self.handleMouseUp}
+//                     onMouseClick={self.handleMouseClick}>{cell}</td>
+//                   )
+//                 })}
+//               </tr>
+//             )
+//           })}
+//           </tbody>
+//         </table>
+//       </div>
+//     );
+//   }
+// }
+// export default Table;
+
 
 var Table = React.createClass({
-  handleClick: function(i) {
-    var allCells= $("#table td");
-    console.log(allCells.index(i.target)+1);
-    console.log(i.target.id);
-    this.props.clickHandler(i);
+  handleMouseMove: function(i) {
+    this.props.moveHandler(i);
   },
-
+  handleMouseUp: function(i) {
+    this.props.upHandler(i);
+  },
+  handleMouseDown: function(i) {
+    this.props.downHandler(i);
+  },
+  handleMouseClick: function(i) {
+    this.props.clickHandler(i)
+  },
   render: function(){
     var self = this;
     var _days = this.props.tdays;
@@ -40,7 +114,11 @@ var Table = React.createClass({
                 <th>{day}</th>
                 {cells.map(function(cell,i){
                   return(
-                    <td id={cell} key={i} onClick={self.handleClick}>{cell}</td>
+                    <td id={cell} key={i}
+                    onMouseMove={self.handleMouseMove}
+                    onMouseDown={self.handleMouseDown}
+                    onMouseUp={self.handleMouseUp}
+                    onMouseClick={self.handleMouseClick}>{cell}</td>
                   )
                 })}
               </tr>
